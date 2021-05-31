4ddce965a9f1f92d26e27f0bee8b08f370952e86
Pavel T <pa.tomilin@gmail.com>
2021-05-31 23:08:55 +0300

    Сделал отрисовку фотографий из массива данных

--- js/main.js
+++ js/main.js
@@ -4,5 +4,28 @@
   const pictureTemplate = document
     .querySelector('#picture')
     .content.querySelector('.picture');
-  console.log(pictureTemplate);
+
+  const renderPhoto = (photos) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = photos.url;
+    pictureElement.querySelector('.picture__comments').textContent =
+      photos.comments.length;
+    pictureElement.querySelector('.picture__likes').textContent = photos.likes;
+
+    return pictureElement;
+  };
+
+  const pictures = document.querySelector('.pictures');
+
+  const render = (photos) => {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < photos.length; i++) {
+      fragment.append(renderPhoto(photos[i]));
+    }
+
+    pictures.append(fragment);
+  };
+  render(window.data.photosArray);
 })();
