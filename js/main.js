8b9590488d2f5202fc69b6113f4d3d45f99d64ba
Paavveel <pa.tomilin@gmail.com>
2021-06-01 12:39:30 +0300

    Сделал открытие карточки фото

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (() => {
   'use strict';
 
+  const body = document.querySelector('body');
   const pictureTemplate = document
     .querySelector('#picture')
     .content.querySelector('.picture');
@@ -28,4 +29,35 @@
     pictures.append(fragment);
   };
   render(window.data.photosArray);
+
+  const bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
+  const bigPicturePreview = bigPicture.querySelector('.big-picture__preview');
+
+  const showPreview = (photos) => {
+    bigPicturePreview.querySelector('.big-picture__img img').src =
+      photos[0].url;
+    bigPicturePreview.querySelector('.social__caption').textContent =
+      photos[0].description;
+    bigPicturePreview.querySelector('.likes-count').textContent =
+      photos[0].likes;
+    bigPicturePreview.querySelector('.comments-count').textContent =
+      photos[0].comments.length;
+    bigPicturePreview.querySelector('.social__comment .social__picture').src =
+      photos[0].comments[0].avatar;
+    bigPicturePreview.querySelector('.social__comment .social__picture').alt =
+      photos[0].comments[0].name;
+    bigPicturePreview.querySelector(
+      '.social__comment .social__text'
+    ).textContent = photos[0].comments[0].message;
+
+    body.classList.add('modal-open');
+  };
+
+  bigPicturePreview
+    .querySelector('.social__comment-count')
+    .classList.add('hidden');
+  bigPicturePreview.querySelector('.comments-loader').classList.add('hidden');
+
+  showPreview(window.data.photosArray);
 })();
